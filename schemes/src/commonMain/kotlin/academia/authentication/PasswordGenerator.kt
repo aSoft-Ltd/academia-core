b3a6dd52dd326267bb9ee9bac2914c3ede55eb8a
andylamax <andylamax@programmer.net>
2024-09-17 14:00:48 +0300

    Before merging with main

--- schemes/src/commonMain/kotlin/academia/authentication/PasswordGenerator.kt
+++ schemes/src/commonMain/kotlin/academia/authentication/PasswordGenerator.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 class PasswordGenerator(
     private val characters: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 ) {
-    fun generate(length: Int = 8): String = buildString {
+    fun generate(length: Int = 6): String = buildString {
         val pool = characters.shuffled(Random(length))
         repeat(length) { append(pool.random()) }
     }

4f9e762c45f62c24960bbcd3d206f0361b039e40
andylamax <andylamax@programmer.net>
2024-09-29 05:39:16 +0300

    Ensuring tests are passing with better attempts

--- schemes/src/commonMain/kotlin/academia/authentication/PasswordGenerator.kt
+++ schemes/src/commonMain/kotlin/academia/authentication/PasswordGenerator.kt
@@ -3,7 +3,7 @@ package academia.authentication
 import kotlin.random.Random
 
 class PasswordGenerator(
-    private val characters: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private val characters: List<Char> = ('A'..'Z') + ('0'..'9')
 ) {
     fun generate(length: Int = 6): String = buildString {
         val pool = characters.shuffled(Random(length))

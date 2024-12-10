c4d7364bd959a758bf4549bfe89ed92045fab94e
andylamax <andylamax@programmer.net>
2024-12-10 06:00:42 +0300

    Moved privacy policy stuff away from core and into the server

---
+++ dtos/src/commonMain/kotlin/academia/legal/privacy/SchoolPrivacyPolicy.kt
@@ -0,0 +1,11 @@
+package academia.legal.privacy
+
+class SchoolPrivacyPolicy(
+    val intro: LegalSection,
+    val general: LegalSection,
+    val students: LegalSection,
+    val guardians: LegalSection,
+    val teachers: LegalSection,
+    val bursars: LegalSection,
+    val deciders: LegalSection
+)

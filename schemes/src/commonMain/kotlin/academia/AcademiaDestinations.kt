2a2647f853f4a6a9d83547ff25959ab14351728c
andylamax <andylamax@programmer.net>
2025-01-16 13:00:17 +0300

    Added multiple routes

--- schemes/src/commonMain/kotlin/academia/AcademiaDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/AcademiaDestinations.kt
@@ -7,13 +7,13 @@ import academia.legal.LegalDestinations
 import academia.registration.RegistrationDestinations
 import academia.website.WebsiteDestinations
 
-class AcademiaDestinations(private val base: String) {
+class AcademiaDestinations(private val base: String) { // e.g. "https://academia.co.tz, http://localhost:9090/v1"
     val outbox by lazy { AcademiaOutboxDestinations("$base/outbox") }
     val registration by lazy { RegistrationDestinations("$base/registration") }
     val authentication by lazy { AuthenticationDestinations("$base/authentication") }
     val legal by lazy { LegalDestinations("$base/legal") }
     val account by lazy { AccountDestinations("$base/account") }
-    val website by lazy { WebsiteDestinations("$base/website") }
+    val website by lazy { WebsiteDestinations(base) }
     val admission by lazy { AdmissionDestinations("$base/admission") }
     fun settings() = "$base/settings"
     fun toBase(base: String) = AcademiaDestinations(base)
--- schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
@@ -1,11 +1,15 @@
 package academia.website
 
-class WebsiteDestinations(private val base: String) {
-    fun home() = base
-    fun about() = "$base/about"
-    fun academic() = "$base/academic"
-    fun admission() = "$base/admission"
-    fun news() = "$base/news"
-    fun fees() = "$base/fees"
-    fun contact() = "$base/contact"
+class WebsiteDestinations(val base: String) {
+    private val root by lazy { "website" }
+    private val prefix by lazy { if (base.isEmpty()) "" else "$base/$root" }
+    fun home() = "$prefix/"
+    fun about() = "$prefix/about"
+    fun academics() = "$prefix/academics"
+    fun admissions() = "$prefix/admissions"
+    fun news() = "$prefix/news"
+    fun fees() = "$prefix/fees"
+    fun contacts() = "$prefix/contacts"
+    fun routes() = "$root/*"
+    fun isolated() = WebsiteDestinations("")
 }

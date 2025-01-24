c50bd13998c9bec3fe4da3f1703e688349e06d2a
andylamax <andylamax@programmer.net>
2025-01-24 19:14:15 +0300

    Befre fetching multi languge support

--- schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
@@ -6,10 +6,20 @@ class WebsiteDestinations(val base: String) {
     fun home() = "$prefix/"
     fun about() = "$prefix/about"
     fun academics() = "$prefix/academics"
+//    val academics by lazy { AcademicsDestinations("$prefix/academics") }
     fun admissions() = "$prefix/admissions"
     fun news() = "$prefix/news"
     fun fees() = "$prefix/fees"
     fun contacts() = "$prefix/contacts"
     fun routes() = "$root/*"
     fun isolated() = WebsiteDestinations("")
+
+    class AcademicsDestinations(private val base: String) {
+        fun index() = base
+        fun hero() = index()
+        fun subjects() = "$base/subjects"
+        fun nursery() = "$base/nursery"
+        fun primary() = "$base/primary"
+        fun routes() = "$base/*"
+    }
 }

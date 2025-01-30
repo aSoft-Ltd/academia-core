efb089e450d785448c075af6398437ed27fe37d3
ronaldchaula <2021-04-01176@udsm.ac.tz>
2025-01-30 22:39:57 +0300

    managed to make routing work on academics landscape and now I am setting it up to work on website home

--- schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/website/WebsiteDestinations.kt
@@ -3,7 +3,7 @@ package academia.website
 class WebsiteDestinations(val base: String) {
     private val root by lazy { "website" }
     private val prefix by lazy { if (base.isEmpty()) "" else "$base/$root" }
-    fun home() = "$prefix/"
+    val home by lazy { HomeDestinations(prefix) }
     fun about() = "$prefix/about"
     val academics by lazy { AcademicsDestinations(prefix) }
     fun admissions() = "$prefix/admissions"
@@ -18,12 +18,26 @@ class WebsiteDestinations(val base: String) {
         private val prefix by lazy { if (base.isEmpty()) "" else "$base/$root" }
         fun index() = "$prefix/"
         fun hero() = index()
-        fun subjects() = "$base/subjects"
-        fun nursery() = "$base/nursery"
-        fun primary() = "$base/primary"
-        fun oLevel() = "$base/oLevel"
-        fun aLevel() = "$base/aLevel"
-        fun extraCurriculum() = "$base/extraCurriculum"
+        fun subjects() = "$prefix/subjects"
+        fun nursery() = "$prefix/nursery"
+        fun primary() = "$prefix/primary"
+        fun oLevel() = "$prefix/oLevel"
+        fun aLevel() = "$prefix/aLevel"
+        fun extraCurriculum() = "$prefix/extraCurriculum"
+        fun routes() = "$root/*"
+        fun isolated() = AcademicsDestinations("")
+    }
+
+    class HomeDestinations(private val base: String) {
+        private val root = "home"
+        private val prefix by lazy { if (base.isEmpty()) "" else "$base/$root" }
+        fun index() = prefix
+        fun hero() = index()
+        fun missionVision() = "$base/missionVision"
+        fun gallery() = "$base/gallery"
+        fun message() = "$base/message"
+        fun news() = "$base/news"
+        fun contact() = "$base/contact"
         fun routes() = "$root/*"
         fun isolated() = AcademicsDestinations("")
     }

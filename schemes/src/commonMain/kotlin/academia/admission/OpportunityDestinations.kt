bc78157f42fb5c0c20a1456d375b11d2c9ee8a18
Isaka Developer <isaxstarisaka@gmail.com>
2025-08-05 18:49:04 +0300

    Admissions opportunity detail layout

---
+++ schemes/src/commonMain/kotlin/academia/admission/OpportunityDestinations.kt
@@ -0,0 +1,14 @@
+package academia.admission
+
+class OpportunityDestinations(prefix: String, private val root: String) {
+    private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
+    fun index() = prefix
+    fun applicants() = "$prefix/applicants"
+    fun limit() = "$prefix/limit"
+    fun payments() = "$prefix/payments"
+    fun results() = "$prefix/results"
+    fun requirements() = "$prefix/requirements"
+    fun advertisements() = "$prefix/advertisement"
+    fun routes() = "$prefix/*"
+    fun isolated() = OpportunityDestinations("", root)
+}

474eecea7f10c7cdfb344094dd940222c86cd18b
ronaldchaula <2021-04-01176@udsm.ac.tz>
2025-11-19 14:51:34 +0300

    touched some things here but they work the same

--- schemes/src/commonMain/kotlin/academia/users/UserDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/users/UserDestinations.kt
@@ -4,7 +4,7 @@ class UserDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun index() = "$prefix/insights"
     fun users() = "$prefix/users"
-    val profile by lazy { UserProfileDestinations("", "user") }
+    val profile by lazy { UserProfileDestinations("", "users") }
     fun roles() = "$prefix/roles"
     fun routes() = "$prefix/*"
     fun isolated() = UserDestinations("", root)

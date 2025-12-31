7e080ecf87cb23a7f60a9622014dbca8ccdd1449
andylamax <andylamax@programmer.net>
2025-12-31 11:40:31 +0300

    Migrated away from koncurrent and kollections

--- schemes/src/commonMain/kotlin/academia/registration/VerificationScheme.kt
+++ schemes/src/commonMain/kotlin/academia/registration/VerificationScheme.kt
@@ -1,22 +1,20 @@
 package academia.registration
 
-import koncurrent.Later
-
 interface VerificationScheme {
     /**
      * Sends the verification code to the set destination provided that they have began the registration process
      * @param account email/phone number of the individual who has begun the registration process
      * @return the account that was passed in or an error if the [account] has not begun registration yet
      */
-    fun sendCode(account: String): Later<StartRegistrationParams>
+    suspend fun sendCode(account: String): StartRegistrationParams
 
     /**
      * Retrieve verification information for the [account] that has begun registration
      */
-    fun info(account: String): Later<StartRegistrationParams>
+    suspend fun info(account: String): StartRegistrationParams
 
     /**
      * Perform the actual verification of with the [code] that has begun registration
      */
-    fun verify(code: String): Later<StartRegistrationParams>
+    suspend fun verify(code: String): StartRegistrationParams
 }

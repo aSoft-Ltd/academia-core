73dffdf3dfd84879f92e2974a340bb2b045daccf
andylamax <andylamax@programmer.net>
2026-01-09 03:35:54 +0300

    Managed to run the entire app from test and managed to hook up to test it locally

--- schemes/src/commonMain/kotlin/academia/registration/Registration.kt
+++ schemes/src/commonMain/kotlin/academia/registration/Registration.kt
@@ -1,5 +1,18 @@
 package academia.registration
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.rpc.annotations.Rpc
+
+@Rpc
 interface Registration {
-    val check: RegistrationCheck
+
+    /**
+     * Suggest school domain names based on the provided school name.
+     */
+    fun suggest(name: Flow<String>): Flow<Suggestion>
+
+    /**
+     * Starts the registration process for a school with the given parameters
+     */
+    suspend fun start(school: SchoolParams): SchoolParams
 }

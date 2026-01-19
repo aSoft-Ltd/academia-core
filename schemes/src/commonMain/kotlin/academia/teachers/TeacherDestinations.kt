48921df070c2ad0cbbcca26cc7c4112b164374cd
robertanold <robert@smartfoundry.co>
2026-01-19 22:35:02 +0300

    fix requested changes

--- schemes/src/commonMain/kotlin/academia/teachers/TeacherDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/teachers/TeacherDestinations.kt
@@ -3,5 +3,5 @@ package academia.teachers
 class TeacherDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun home() = prefix
-    fun teachers() = "$prefix/list"
+    fun list() = "$prefix/list"
 }

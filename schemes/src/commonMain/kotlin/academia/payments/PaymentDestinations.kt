7f7cc1ef2c7c5e5ed62dca0a615587d4a42b8e52
Isaka Developer <isaxstarisaka@gmail.com>
2026-01-31 23:51:43 +0300

    payment transaction routes

--- schemes/src/commonMain/kotlin/academia/payments/PaymentDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/payments/PaymentDestinations.kt
@@ -3,6 +3,7 @@ package academia.payments
 class PaymentDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     val wallet by lazy { Wallet(this.prefix, "wallet") }
+    val transaction by lazy { Transaction(this.prefix, "transaction") }
     fun dashboard() = "$prefix/dashboard"
     fun wallets() = "$prefix/wallets"
     fun invoices() = "$prefix/invoices"
@@ -17,5 +18,12 @@ class PaymentDestinations(prefix: String, private val root: String) {
         fun transactions() = "$prefix/transactions"
         fun routes() = "$root/*"
         fun isolated() = Wallet("", root)
+    }
+
+    class Transaction(prefix: String, private val root: String) {
+        private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
+        fun info() = "$prefix/info"
+        fun routes() = "$root/*"
+        fun isolated() = Transaction("", root)
     }
 }

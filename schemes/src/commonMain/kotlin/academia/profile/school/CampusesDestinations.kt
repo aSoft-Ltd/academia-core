72ff50a8ea9e136643d0013f9d56586e4ca3b189
andylamax <andylamax@programmer.net>
2026-02-23 08:47:43 +0300

    Added nested routing with conditional navigation

--- schemes/src/commonMain/kotlin/academia/profile/school/CampusesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/profile/school/CampusesDestinations.kt
@@ -11,8 +11,17 @@ class CampusesDestinations(prefix: String, private val root: String) {
     class CampusDestinations(prefix: String, private val root: String) {
         private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
         fun index() = prefix
-        fun curriculum() = "$prefix/curriculum"
+        fun curriculum() = CurriculaDestinations(prefix, "curricula")
         fun routes() = "$root/*"
         fun isolated() = CampusDestinations("", root)
+
+        class CurriculaDestinations(prefix: String, private val root: String) {
+            private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
+            fun index() = prefix
+            fun routes() = "$root/*"
+            fun list() = prefix
+            fun uid(uid: String) = "$prefix/$uid"
+            fun isolated() = CurriculaDestinations("", root)
+        }
     }
 }

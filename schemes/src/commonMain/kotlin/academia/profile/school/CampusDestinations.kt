87169d6c11d7185f16e75463efbc4cd2acd3eae9
andylamax <andylamax@programmer.net>
2026-02-26 05:46:56 +0300

    Added some routes for campus to see how to hadle non nested navigations

--- schemes/src/commonMain/kotlin/academia/profile/school/CampusDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/profile/school/CampusDestinations.kt
@@ -4,6 +4,12 @@ class CampusDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun index() = "/$prefix"
     fun curriculums() = CurriculumsDestinations(prefix, "curriculums")
+    fun contacts() = "$prefix/contacts"
+    fun socials() = "$prefix/socials"
+    fun payments() = "$prefix/payments"
+    fun credits() = "$prefix/credits"
+    fun rules() = "$prefix/rules"
+    fun legal() = "$prefix/legal"
     fun routes() = "$root/*"
     fun isolated() = CampusDestinations("", root)
 }

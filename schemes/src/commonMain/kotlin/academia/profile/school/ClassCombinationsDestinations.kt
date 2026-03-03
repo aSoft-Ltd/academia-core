cba4c1a6f50be294a91153854cc51ee9a34270b8
robertanold <robert@smartfoundry.co>
2026-03-03 11:40:30 +0300

    Add combination and stream routes

---
+++ schemes/src/commonMain/kotlin/academia/profile/school/ClassCombinationsDestinations.kt
@@ -0,0 +1,8 @@
+package academia.profile.school
+
+class ClassCombinationsDestinations(prefix: String, private val root: String) {
+    private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
+    fun index() = prefix
+    fun routes() = "$root/*"
+    fun isolated() = ClassCombinationsDestinations("", root)
+}

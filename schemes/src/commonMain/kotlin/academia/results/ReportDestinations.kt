5a120146e316823d42fd8b914c2a75bea73ae1ca
andylamax <andylamax@programmer.net>
2026-04-02 14:38:47 +0300

    added routes directive

--- schemes/src/commonMain/kotlin/academia/results/ReportDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/results/ReportDestinations.kt
@@ -3,6 +3,9 @@ package academia.results
 class ReportDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun index() = prefix
-    fun report(uid: String) = ReportDestinations(this.prefix, uid)
+    fun routes() = "$root/*"
+    fun summary() = "$prefix/summary"
+    fun sitters() = "$prefix/sitters"
+    fun results() = "$prefix/results"
     fun isolated() = ReportDestinations("", root)
 }

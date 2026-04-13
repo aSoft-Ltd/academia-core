cc56660cc9d13d287d3290aa4a304362fe09ee7a
andylamax <andylamax@programmer.net>
2026-04-13 16:18:07 +0300

    Removed authentication scheme for academia

--- schemes/src/commonMain/kotlin/academia/AcademiaScheme.kt
+++ schemes/src/commonMain/kotlin/academia/AcademiaScheme.kt
@@ -1,13 +1,8 @@
 package academia
 
-import academia.authentication.AuthenticationScheme
-import academia.registration.RegistrationScheme
-
 interface AcademiaScheme {
-    @Deprecated("Use 'auth' property instead", ReplaceWith("auth"))
-    val authentication: AuthenticationScheme
 
     // TODO: Consider renaming to 'authentication' after we have removed authentication for consistency
     val auth: Auth
-    val registration: RegistrationScheme
+//    val registration: RegistrationScheme
 }

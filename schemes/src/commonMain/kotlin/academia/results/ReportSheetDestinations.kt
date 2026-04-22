d6815d9959e3f34d468b12c219bb352e81285073
robertanold <robert@smartfoundry.co>
2026-04-22 11:59:02 +0300

    Add stream route

--- schemes/src/commonMain/kotlin/academia/results/ReportSheetDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/results/ReportSheetDestinations.kt
@@ -7,6 +7,7 @@ class ReportSheetDestinations(prefix: String, private val root: String) {
     fun sitters() = ReportSitterDestinations(this.prefix, "sitters")
     fun results() = ReportResultDestinations(this.prefix, "results")
     fun courses() = ReportCourseDestinations(this.prefix, "courses")
+    fun streams() = ReportStreamsDestinations(this.prefix, "streams")
     fun routes() = "$root/*"
     fun isolated() = ReportSheetDestinations("", root)
 }

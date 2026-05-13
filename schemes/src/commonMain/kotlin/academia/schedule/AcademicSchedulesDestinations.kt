a9ec49ed9d2fd0b21a7b27ec5317fdb75fcf9d4b
robertanold <robert@smartfoundry.co>
2026-05-13 09:37:41 +0300

    fix requested changes

--- schemes/src/commonMain/kotlin/academia/schedule/AcademicSchedulesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/schedule/AcademicSchedulesDestinations.kt
@@ -1,6 +1,5 @@
 package academia.schedule
 
-
 class AcademicSchedulesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun index() = prefix

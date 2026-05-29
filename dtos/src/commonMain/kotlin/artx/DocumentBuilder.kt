2cfd92b4f599a81eec696590905ad9547e42896a
andylamax <andylamax@programmer.net>
2026-05-29 09:07:54 +0300

    Improved packaging structure

--- dtos/src/commonMain/kotlin/artx/DocumentBuilder.kt
+++ dtos/src/commonMain/kotlin/artx/DocumentBuilder.kt
@@ -13,12 +13,12 @@ class DocumentBuilder(internal val depth: Int = 0) {
         current: artx.DocumentBuilder.() -> Unit,
         children: artx.DocumentBuilder.() -> Unit
     ): artx.elements.FertileElement {
-        val curr = _root_ide_package_.artx.DocumentBuilder(depth).apply(current).elements
-        val kids = _root_ide_package_.artx.DocumentBuilder(depth + 1).apply(children).elements
-        val component = _root_ide_package_.artx.elements.internal.FertileComponentImpl(kind, depth, curr + kids)
+        val curr = DocumentBuilder(depth).apply(current).elements
+        val kids = DocumentBuilder(depth + 1).apply(children).elements
+        val component = artx.elements.internal.FertileComponentImpl(kind, depth, curr + kids)
         elements.add(component)
         return component
     }
 
-    internal fun build() = _root_ide_package_.artx.Document(elements)
+    internal fun build() = artx.Document(elements)
 }

a0d7e106fb1d1937e4bc7f4655bbce0a0a0d6677
neicore <neicoreadams@gmail.com>
2026-07-15 14:39:01 +0300

    improve naming

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -2,7 +2,7 @@ package academia.classes
 
 class ClassesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
-    fun moduleSetup() = prefix
+    fun dashboard() = prefix
     fun routes() = "$root/*"
     fun isolated() = ClassesDestinations("", root)
 }

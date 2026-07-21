89c0f4c43364b78a5dbed22f9f2dc0bd3013798b
neicore <neicoreadams@gmail.com>
2026-07-21 12:24:53 +0300

    add students endpoint in in classes

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -4,6 +4,7 @@ class ClassesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun dashboard() = prefix
     fun profile(uid: String) = if (prefix.isEmpty()) uid else "$prefix/$uid"
+    fun profileStudents(uid: String) = "${profile(uid)}/students"
     fun routes() = "$root/*"
     fun isolated() = ClassesDestinations("", root)
 }

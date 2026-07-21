d0ae3fb8e0ef1746a4facd99bd453badcdbf8e08
neicore <neicoreadams@gmail.com>
2026-07-22 01:15:21 +0300

    improve class routes implementation

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -2,9 +2,20 @@ package academia.classes
 
 class ClassesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
-    fun dashboard() = prefix
-    fun profile(uid: String) = if (prefix.isEmpty()) uid else "$prefix/$uid"
-    fun profileStudents(uid: String) = "${profile(uid)}/students"
+    fun index() = prefix
+    fun `class`(uid: String) = ClassDestinations(this.prefix, uid)
     fun routes() = "$root/*"
     fun isolated() = ClassesDestinations("", root)
+
+    class ClassDestinations(prefix: String, private val root: String) {
+        private val prefix by lazy { if (prefix.isEmpty()) root else "$prefix/$root" }
+        fun index() = prefix
+        fun students() = "$prefix/students"
+        fun streams() = "$prefix/streams"
+        fun subjects() = "$prefix/subjects"
+        fun teachers() = "$prefix/teachers"
+        fun attendance() = "$prefix/attendance"
+        fun routes() = "$root/*"
+        fun isolated() = ClassDestinations("", root)
+    }
 }

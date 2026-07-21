b218ce5f7fe94e8db614693d33e337cbcaada775
neicore <neicoreadams@gmail.com>
2026-07-21 10:03:48 +0300

    add single class route

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -3,6 +3,7 @@ package academia.classes
 class ClassesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun dashboard() = prefix
+    fun profile(uid: String) = if (prefix.isEmpty()) uid else "$prefix/$uid"
     fun routes() = "$root/*"
     fun isolated() = ClassesDestinations("", root)
 }

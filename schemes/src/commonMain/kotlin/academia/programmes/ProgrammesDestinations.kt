b5050afbb62949e7f209ca86d3af309a886fd4ec
robertanold <robert@smartfoundry.co>
2026-07-28 19:00:20 +0300

    add manage programmes routes

--- schemes/src/commonMain/kotlin/academia/programmes/ProgrammesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/programmes/ProgrammesDestinations.kt
@@ -3,6 +3,7 @@ package academia.programmes
 class ProgrammesDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun home() = prefix
+    fun programmes() = "$prefix/programmes"
     fun routes() = "$root/*"
     fun isolated() = ProgrammesDestinations("", root)
 }

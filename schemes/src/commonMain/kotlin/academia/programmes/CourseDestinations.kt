7681f4547e8e412950c2d8344f05edefafca22d6
robertanold <robert@smartfoundry.co>
2026-08-04 17:41:31 +0300

    add courses semester route

---
+++ schemes/src/commonMain/kotlin/academia/programmes/CourseDestinations.kt
@@ -0,0 +1,9 @@
+package academia.programmes
+
+class CourseDestinations(prefix: String, private val root: String) {
+    private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
+    fun index() = prefix
+    fun semester(semester: String) = "$prefix/$semester"
+    fun routes() = "$root/*"
+    fun isolated() = CourseDestinations("", root)
+}

17f19c6a490c7441398beaf74086201451367889
neicore <neicoreadams@gmail.com>
2026-08-05 18:01:54 +0300

    add subject destinations

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -14,10 +14,23 @@ class ClassesDestinations(prefix: String, private val root: String) {
         fun streams() = "$prefix/streams"
         fun stream(uid: String) = StreamDestinations(this.prefix, "streams/$uid")
         fun subjects() = "$prefix/subjects"
+        fun subject(uid: String) = SubjectDestinations(this.prefix, "subjects/$uid")
         fun teachers() = "$prefix/teachers"
         fun attendance() = "$prefix/attendance"
         fun routes() = "$root/*"
         fun isolated() = ClassDestinations("", root)
+
+        class SubjectDestinations(prefix: String, private val root: String) {
+            private val prefix by lazy { if (prefix.isEmpty()) root else "$prefix/$root" }
+            fun index() = prefix
+            fun students() = "$prefix/students"
+            fun topics() = "$prefix/topics"
+            fun periods() = "$prefix/periods"
+            fun attendance() = "$prefix/attendance"
+            fun questions() = "$prefix/questions"
+            fun routes() = "$root/*"
+            fun isolated() = SubjectDestinations("", root)
+        }
 
         class StreamDestinations(prefix: String, private val root: String) {
             private val prefix by lazy { if (prefix.isEmpty()) root else "$prefix/$root" }

ca39088f8740a9c80a532d67fd49daed8f4afbdb
robertanold <robert@smartfoundry.co>
2026-08-12 16:23:56 +0300

    Add combination single view routes

--- schemes/src/commonMain/kotlin/academia/combination/CombinationDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/combination/CombinationDestinations.kt
@@ -2,8 +2,7 @@ package academia.combination
 
 class CombinationDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
-    fun home() = prefix
-    fun combinations() = "$prefix/combinations"
+    fun index() = prefix
     fun routes() = "$root/*"
     fun isolated() = CombinationDestinations("", root)
 }

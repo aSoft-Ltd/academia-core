d3a597e8cba87ce5e065431a9350ec87f280e6f6
robertanold <robert@smartfoundry.co>
2026-08-18 18:21:59 +0300

    Add attendance route in combination

--- schemes/src/commonMain/kotlin/academia/combination/CombinationLevelDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/combination/CombinationLevelDestinations.kt
@@ -1,9 +1,11 @@
 package academia.combination
 
+import academia.attendance.AttendancesDestinations
+
 class CombinationLevelDestinations(prefix: String, private val root: String) {
     private val prefix by lazy { if (prefix.isEmpty()) "" else "$prefix/$root" }
     fun students() = "$prefix/students"
-    fun attendance() = "$prefix/attendance"
+    fun attendance() = AttendancesDestinations(prefix, "attendance")
     fun subjects() = "$prefix/subjects"
     fun teachers() = "$prefix/teachers"
     fun streams() = "$prefix/streams"

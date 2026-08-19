2b13634962c84b24f2a423d137e8af086cec98d4
neicore <neicoreadams@gmail.com>
2026-08-19 22:58:23 +0300

    improve perid routes

--- schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
+++ schemes/src/commonMain/kotlin/academia/classes/ClassesDestinations.kt
@@ -39,9 +39,16 @@ class ClassesDestinations(prefix: String, private val root: String) {
             class PeriodDestinations(prefix: String, private val root: String) {
                 private val prefix by lazy { if (prefix.isEmpty()) root else "$prefix/$root" }
                 fun index() = prefix
-                fun plan() = "$prefix/plan"
+                fun plan() = PlanDestinations(this.prefix, "plan")
                 fun routes() = "$root/*"
                 fun isolated() = PeriodDestinations("", root)
+
+                class PlanDestinations(prefix: String, private val root: String) {
+                    private val prefix by lazy { if (prefix.isEmpty()) root else "$prefix/$root" }
+                    fun index() = prefix
+                    fun routes() = "$root/*"
+                    fun isolated() = PlanDestinations("", root)
+                }
             }
 
             class TopicDestinations(prefix: String, private val root: String) {
